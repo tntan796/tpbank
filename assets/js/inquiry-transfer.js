07d90cc2d82929a9929b265a45bc2c0c292dc264
Tan Tano <tantn1@sunshinegroup.vn>
2021-03-03 10:46:05 +0700

    inquiry transfer

---
+++ assets/js/inquiry-transfer.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+
+    // // Handle event action when click transferType
+    // const transferType = document.getElementById('transfer-type');
+    // const transferTypePopup = document.getElementsByClassName('search-by-catalogue')[0];
+    // const btnCloseTransferType = document.getElementsByClassName('btn-close-transfer-type')[0];
+    // transferType.addEventListener('click', function() {
+    //     transferTypePopup.classList.add('show');
+    // })
+    // btnCloseTransferType.addEventListener('click', function() {
+    //     transferTypePopup.classList.remove('show');
+    // })
+
+
+    // // Show detail when click transaction
+    // const contentTransactions = document.querySelectorAll('.content-transaction');
+    // contentTransactions.forEach((transaction, index) => {
+    //     transaction.addEventListener('click', function(index) {
+    //         alert(index);
+    //     });
+    // })
+});
